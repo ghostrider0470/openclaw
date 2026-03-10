dadasersin/openclaw-yapay-zeka-asistan-

feat(azure-foundry): add Azure AI Foundry / Azure OpenAI provider

src/agents/azure-foundry-discovery.ts
@@ -1,0 +1,230 @@
+import type { ModelDefinitionConfig } from "../config/types.models.js";
+import { createSubsystemLogger } from "../logging/subsystem.js";
+import { AZURE_FOUNDRY_ANTHROPIC_API_VERSION, isAnthropicModelId } from "./azure-foundry-models.js";
+
+const log = createSubsystemLogger("azure-foundry-discovery");
+
+const DEFAULT_REFRESH_INTERVAL_SECONDS = 3600;
+const DEFAULT_CONTEXT_WINDOW = 32000;
+const DEFAULT_MAX_TOKENS = 4096;
+const DEFAULT_COST = {
+  input: 0,
+  output: 0,
+  cacheRead: 0,
+  cacheWrite: 0,
+};
+
+import type { AzureFoundryDiscoveryConfig } from "../config/types.models.js";
+export type { AzureFoundryDiscoveryConfig };
+
+type AzureFoundryModelEntry = {
+  id?: string;
+  name?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+};
+
+type AzureFoundryDiscoveryCacheEntry = {
+  expiresAt: number;
+  value?: ModelDefinitionConfig[];
+  inFlight?: Promise<ModelDefinitionConfig[]>;
+};
+
+const discoveryCache = new Map<string, AzureFoundryDiscoveryCacheEntry>();
+let hasLoggedAzureFoundryError = false;
+
+function normalizeProviderFilter(filter?: string[]): string[] {
+  if (!filter || filter.length === 0) {
+    return [];
+  }
+  const normalized = new Set(
+    filter.map((entry) => entry.trim().toLowerCase()).filter((entry) => entry.length > 0),
+  );
+  return Array.from(normalized).toSorted();
+}
+
+function buildCacheKey(params: {
+  endpoint: string;
+  apiKeyHash: string;
+  providerFilter: string[];
+  refreshIntervalSeconds: number;
+  defaultContextWindow: number;
+  defaultMaxTokens: number;
+}): string {
+  return JSON.stringify(params);
+}
+
+/** Produce a short hash of the API key for cache scoping (avoids storing the full key). */
+function hashApiKey(apiKey: string): string {
+  if (!apiKey) {
+    return "empty";
+  }
+  // Use a simple prefix+suffix fingerprint; not cryptographic but sufficient for cache scoping.
+  return `${apiKey.slice(0, 4)}…${apiKey.slice(-4)}:${apiKey.length}`;
+}
+
+function matchesProviderFilter(modelId: string, filter: string[]): boolean {
+  if (filter.length === 0) {
+    return true;
+  }
+  const normalized = modelId.trim().toLowerCase();
+  return filter.some((f) => normalized.includes(f));
+}
+
+function inferReasoningSupport(modelId: string, modelName: string): boolean {
+  const haystack = `${modelId} ${modelName}`.toLowerCase();
+  return (
+    haystack.includes("reasoning") || haystack.includes("thinking") || /\bo[34]-/.test(haystack)
+  );
+}
+
+function inferInputModalities(modelId: string, modelName: string): Array<"text" | "image"> {
+  const haystack = `${modelId} ${modelName}`.toLowerCase();
+  if (
+    haystack.includes("gpt-4o") ||
+    haystack.includes("gpt-4.1") ||
+    haystack.includes("vision") ||
+    haystack.includes("claude")
+  ) {
+    return ["text", "image"];
+  }
+  return ["text"];
+}
+
+function resolveDefaultContextWindow(config?: AzureFoundryDiscoveryConfig): number {
+  const value = Math.floor(config?.defaultContextWindow ?? DEFAULT_CONTEXT_WINDOW);
+  return value > 0 ? value : DEFAULT_CONTEXT_WINDOW;
+}
+
+function resolveDefaultMaxTokens(config?: AzureFoundryDiscoveryConfig): number {
+  const value = Math.floor(config?.defaultMaxTokens ?? DEFAULT_MAX_TOKENS);
+  return value > 0 ? value : DEFAULT_MAX_TOKENS;
+}
+
+function toModelDefinition(
+  entry: AzureFoundryModelEntry,
+  defaults: { contextWindow: number; maxTokens: number; endpoint: string },
+): ModelDefinitionConfig | null {
+  const id = entry.id?.trim();
+  if (!id) {
+    return null;
+  }
+  const name = entry.name?.trim() || id;
+  const base: ModelDefinitionConfig = {
+    id,
+    name,
+    reasoning: inferReasoningSupport(id, name),
+    input: inferInputModalities(id, name),
+    cost: DEFAULT_COST,
+    contextWindow: defaults.contextWindow,
+    maxTokens: defaults.maxTokens,
+  };
+  // Route Anthropic (Claude) models to the /anthropic endpoint
+  if (isAnthropicModelId(id)) {
+    base.api = "anthropic-messages";
+    base.baseUrl = `${defaults.endpoint}/anthropic`;
+    base.headers = { "api-version": AZURE_FOUNDRY_ANTHROPIC_API_VERSION };
+  }
+  return base;
+}
+
+export function resetAzureFoundryDiscoveryCacheForTest(): void {
+  discoveryCache.clear();
+  hasLoggedAzureFoundryError = false;
+}
+
+export async function discoverAzureFoundryModels(params: {
+  endpoint: string;
+  apiKey: string;
+  config?: AzureFoundryDiscoveryConfig;
+  now?: () => number;
+  fetchFn?: typeof fetch;
+}): Promise<ModelDefinitionConfig[]> {
+  const refreshIntervalSeconds = Math.max(
+    0,
+    Math.floor(params.config?.refreshInterval ?? DEFAULT_REFRESH_INTERVAL_SECONDS),
+  );
+  const providerFilter = normalizeProviderFilter(params.config?.providerFilter);
+  const defaultContextWindow = resolveDefaultContextWindow(params.config);
+  const defaultMaxTokens = resolveDefaultMaxTokens(params.config);
+  const cacheKey = buildCacheKey({
+    endpoint: params.endpoint,
+    apiKeyHash: hashApiKey(params.apiKey),
+    providerFilter,
+    refreshIntervalSeconds,
+    defaultContextWindow,
+    defaultMaxTokens,
+  });
+  const now = params.now?.() ?? Date.now();
+
+  if (refreshIntervalSeconds > 0) {
+    const cached = discoveryCache.get(cacheKey);
+    if (cached?.value && cached.expiresAt > now) {
+      return cached.value;
+    }
+    if (cached?.inFlight) {
+      return cached.inFlight;
+    }
+  }
+
+  const fetchFn = params.fetchFn ?? fetch;
+  const endpoint = params.endpoint.replace(/\/+$/, "");
+  const url = `${endpoint}/models?api-version=2024-05-01-preview`;
+
+  const discoveryPromise = (async () => {
+    const response = await fetchFn(url, {
+      method: "GET",
+      headers: {
+        "api-key": params.apiKey,
+        Accept: "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Azure Foundry model listing failed (${response.status})`);
+    }
+    const body = (await response.json()) as { data?: AzureFoundryModelEntry[] };
+    const entries = body.data ?? [];
+    const discovered: ModelDefinitionConfig[] = [];
+    for (const entry of entries) {
+      if (!matchesProviderFilter(entry.id ?? "", providerFilter)) {
+        continue;
+      }
+      const def = toModelDefinition(entry, {
+        contextWindow: defaultContextWindow,
+        maxTokens: defaultMaxTokens,
+        endpoint,
+      });
+      if (def) {
+        discovered.push(def);
+      }
+    }
+    return discovered.toSorted((a, b) => a.name.localeCompare(b.name));
+  })();
+
+  if (refreshIntervalSeconds > 0) {
+    discoveryCache.set(cacheKey, {
+      expiresAt: now + refreshIntervalSeconds * 1000,
+      inFlight: discoveryPromise,
+    });
+  }
+
+  try {
+    const value = await discoveryPromise;
+    if (refreshIntervalSeconds > 0) {
+      discoveryCache.set(cacheKey, {
+        expiresAt: now + refreshIntervalSeconds * 1000,
+        value,
+      });
+    }
+    return value;
+  } catch (error) {
+    if (refreshIntervalSeconds > 0) {
+      discoveryCache.delete(cacheKey);
+    }
+    if (!hasLoggedAzureFoundryError) {
+      hasLoggedAzureFoundryError = true;
+      log.warn(`Failed to list Azure Foundry models: ${String(error)}`);
+    }
+    return [];
+  }
+}

src/media-understanding/providers/azure/audio.ts
@@ -1,0 +1,113 @@
+import { createSubsystemLogger } from "../../../logging/subsystem.js";
+import { resolveAzureFoundryApiVersionEnv } from "../../../providers/azure-foundry/env.js";
+import type { AudioTranscriptionRequest, AudioTranscriptionResult } from "../../types.js";
+import {
+  assertOkOrThrowHttpError,
+  normalizeBaseUrl,
+  postTranscriptionRequest,
+  requireTranscriptionText,
+} from "../shared.js";
+
+const log = createSubsystemLogger("azure-audio");
+
+const DEFAULT_AZURE_AUDIO_BASE_URL = "https://models.inference.ai.azure.com";
+const DEFAULT_AZURE_AUDIO_MODEL = "whisper-large-v3-turbo";
+const DEFAULT_AZURE_API_VERSION = "2025-04-01-preview";
+
+function resolveModel(model?: string): string {
+  const trimmed = model?.trim();
+  return trimmed || DEFAULT_AZURE_AUDIO_MODEL;
+}
+
+/**
+ * Build the transcription URL for Azure Foundry.
+ *
+ * Two forms are supported:
+ *
+ * 1. **Full deployment URL** (baseUrl already contains `/openai/deployments/{name}`):
+ *    → append `/audio/transcriptions`
+ *
+ * 2. **Bare endpoint** (e.g. `https://models.inference.ai.azure.com`):
+ *    → append `/openai/deployments/{model}/audio/transcriptions`
+ */
+function buildTranscriptionUrl(baseUrl: string, model: string): URL {
+  if (/\/openai\/deployments\/[^/]+\/?$/i.test(baseUrl)) {
+    // baseUrl already points to a specific deployment.
+    return new URL(`${baseUrl.replace(/\/+$/, "")}/audio/transcriptions`);
+  }
+  return new URL(`${baseUrl}/openai/deployments/${encodeURIComponent(model)}/audio/transcriptions`);
+}
+
+export async function transcribeAzureAudio(
+  params: AudioTranscriptionRequest,
+): Promise<AudioTranscriptionResult> {
+  const fetchFn = params.fetchFn ?? fetch;
+  const baseUrl = normalizeBaseUrl(params.baseUrl, DEFAULT_AZURE_AUDIO_BASE_URL);
+  const allowPrivate = Boolean(params.baseUrl?.trim());
+
+  const model = resolveModel(params.model);
+  const url = buildTranscriptionUrl(baseUrl, model);
+
+  // Apply query params from providerOptions (e.g. api-version).
+  if (params.query) {
+    for (const [key, value] of Object.entries(params.query)) {
+      if (value === undefined) {
+        continue;
+      }
+      url.searchParams.set(key, String(value));
+    }
+  }
+  // Ensure api-version is always present.
+  if (!url.searchParams.has("api-version")) {
+    const apiVersion =
+      resolveAzureFoundryApiVersionEnv(process.env)?.value || DEFAULT_AZURE_API_VERSION;
+    url.searchParams.set("api-version", apiVersion);
+  }
+
+  const form = new FormData();
+  const fileName = params.fileName?.trim() || "audio";
+  const bytes = new Uint8Array(params.buffer);
+  const blob = new Blob([bytes], {
+    type: params.mime ?? "application/octet-stream",
+  });
+  form.append("file", blob, fileName);
+  form.append("model", model);
+  if (params.language?.trim()) {
+    form.append("language", params.language.trim());
+  }
+  if (params.prompt?.trim()) {
+    form.append("prompt", params.prompt.trim());
+  }
+
+  const headers = new Headers(params.headers);
+  // Remove any Bearer auth that may have been set by the caller.
+  headers.delete("authorization");
+  // Azure OpenAI transcription endpoints accept api-key (and optional Bearer token),
+  // including cognitiveservices and services.ai hosts.
+  headers.set("api-key", params.apiKey);
+
+  const { response: res, release } = await postTranscriptionRequest({
+    url: url.toString(),
+    headers,
+    body: form,
+    timeoutMs: params.timeoutMs,
+    fetchFn,
+    allowPrivateNetwork: allowPrivate,
+  });
+
+  try {
+    await assertOkOrThrowHttpError(res, "Azure audio transcription failed");
+
+    const payload = (await res.json()) as { text?: string };
+    const text = requireTranscriptionText(
+      payload.text,
+      "Azure audio transcription response missing text",
+    );
+    return { text, model };
+  } catch (err) {
+    log.warn(`Transcription failed for ${url.origin}${url.pathname}: ${String(err)}`);
+    throw err;
+  } finally {
+    await release();
+  }
+}

ghostrider0470/openclaw

src/agents/azure-foundry-discovery.ts
@@ -1,0 +1,234 @@
+import type { ModelDefinitionConfig } from "../config/types.models.js";
+import { createSubsystemLogger } from "../logging/subsystem.js";
+import { AZURE_FOUNDRY_ANTHROPIC_API_VERSION, isAnthropicModelId } from "./azure-foundry-models.js";
+
+const log = createSubsystemLogger("azure-foundry-discovery");
+
+const DEFAULT_REFRESH_INTERVAL_SECONDS = 3600;
+const DEFAULT_CONTEXT_WINDOW = 32000;
+const DEFAULT_MAX_TOKENS = 4096;
+const DEFAULT_COST = {
+  input: 0,
+  output: 0,
+  cacheRead: 0,
+  cacheWrite: 0,
+};
+
+import type { AzureFoundryDiscoveryConfig } from "../config/types.models.js";
+export type { AzureFoundryDiscoveryConfig };
+
+type AzureFoundryModelEntry = {
+  id?: string;
+  name?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+};
+
+type AzureFoundryDiscoveryCacheEntry = {
+  expiresAt: number;
+  value?: ModelDefinitionConfig[];
+  inFlight?: Promise<ModelDefinitionConfig[]>;
+};
+
+const discoveryCache = new Map<string, AzureFoundryDiscoveryCacheEntry>();
+let hasLoggedAzureFoundryError = false;
+
+function normalizeProviderFilter(filter?: string[]): string[] {
+  if (!filter || filter.length === 0) {
+    return [];
+  }
+  const normalized = new Set(
+    filter.map((entry) => entry.trim().toLowerCase()).filter((entry) => entry.length > 0),
+  );
+  return Array.from(normalized).toSorted();
+}
+
+function buildCacheKey(params: {
+  endpoint: string;
+  apiKeyHash: string;
+  providerFilter: string[];
+  refreshIntervalSeconds: number;
+  defaultContextWindow: number;
+  defaultMaxTokens: number;
+}): string {
+  return JSON.stringify(params);
+}
+
+/** Produce a short hash of the API key for cache scoping (avoids storing the full key). */
+function hashApiKey(apiKey: string): string {
+  if (!apiKey) {
+    return "empty";
+  }
+  // Use a simple prefix+suffix fingerprint; not cryptographic but sufficient for cache scoping.
+  return `${apiKey.slice(0, 4)}…${apiKey.slice(-4)}:${apiKey.length}`;
+}
+
+function matchesProviderFilter(entry: AzureFoundryModelEntry, filter: string[]): boolean {
+  if (filter.length === 0) {
+    return true;
+  }
+  // Match against all available metadata: id, name, owned_by, publisher.
+  const haystack = [entry.id, entry.name, entry.owned_by, entry.publisher]
+    .filter(Boolean)
+    .join(" ")
+    .toLowerCase();
+  return filter.some((f) => haystack.includes(f));
+}
+
+function inferReasoningSupport(modelId: string, modelName: string): boolean {
+  const haystack = `${modelId} ${modelName}`.toLowerCase();
+  return (
+    haystack.includes("reasoning") || haystack.includes("thinking") || /\bo[34]-/.test(haystack)
+  );
+}
+
+function inferInputModalities(modelId: string, modelName: string): Array<"text" | "image"> {
+  const haystack = `${modelId} ${modelName}`.toLowerCase();
+  if (
+    haystack.includes("gpt-4o") ||
+    haystack.includes("gpt-4.1") ||
+    haystack.includes("vision") ||
+    haystack.includes("claude")
+  ) {
+    return ["text", "image"];
+  }
+  return ["text"];
+}
+
+function resolveDefaultContextWindow(config?: AzureFoundryDiscoveryConfig): number {
+  const value = Math.floor(config?.defaultContextWindow ?? DEFAULT_CONTEXT_WINDOW);
+  return value > 0 ? value : DEFAULT_CONTEXT_WINDOW;
+}
+
+function resolveDefaultMaxTokens(config?: AzureFoundryDiscoveryConfig): number {
+  const value = Math.floor(config?.defaultMaxTokens ?? DEFAULT_MAX_TOKENS);
+  return value > 0 ? value : DEFAULT_MAX_TOKENS;
+}
+
+function toModelDefinition(
+  entry: AzureFoundryModelEntry,
+  defaults: { contextWindow: number; maxTokens: number; endpoint: string },
+): ModelDefinitionConfig | null {
+  const id = entry.id?.trim();
+  if (!id) {
+    return null;
+  }
+  const name = entry.name?.trim() || id;
+  const base: ModelDefinitionConfig = {
+    id,
+    name,
+    reasoning: inferReasoningSupport(id, name),
+    input: inferInputModalities(id, name),
+    cost: DEFAULT_COST,
+    contextWindow: defaults.contextWindow,
+    maxTokens: defaults.maxTokens,
+  };
+  // Route Anthropic (Claude) models to the /anthropic endpoint
+  if (isAnthropicModelId(id)) {
+    base.api = "anthropic-messages";
+    base.baseUrl = `${defaults.endpoint}/anthropic`;
+    base.headers = { "api-version": AZURE_FOUNDRY_ANTHROPIC_API_VERSION };
+  }
+  return base;
+}
+
+export function resetAzureFoundryDiscoveryCacheForTest(): void {
+  discoveryCache.clear();
+  hasLoggedAzureFoundryError = false;
+}
+
+export async function discoverAzureFoundryModels(params: {
+  endpoint: string;
+  apiKey: string;
+  config?: AzureFoundryDiscoveryConfig;
+  now?: () => number;
+  fetchFn?: typeof fetch;
+}): Promise<ModelDefinitionConfig[]> {
+  const refreshIntervalSeconds = Math.max(
+    0,
+    Math.floor(params.config?.refreshInterval ?? DEFAULT_REFRESH_INTERVAL_SECONDS),
+  );
+  const providerFilter = normalizeProviderFilter(params.config?.providerFilter);
+  const defaultContextWindow = resolveDefaultContextWindow(params.config);
+  const defaultMaxTokens = resolveDefaultMaxTokens(params.config);
+  const cacheKey = buildCacheKey({
+    endpoint: params.endpoint,
+    apiKeyHash: hashApiKey(params.apiKey),
+    providerFilter,
+    refreshIntervalSeconds,
+    defaultContextWindow,
+    defaultMaxTokens,
+  });
+  const now = params.now?.() ?? Date.now();
+
+  if (refreshIntervalSeconds > 0) {
+    const cached = discoveryCache.get(cacheKey);
+    if (cached?.value && cached.expiresAt > now) {
+      return cached.value;
+    }
+    if (cached?.inFlight) {
+      return cached.inFlight;
+    }
+  }
+
+  const fetchFn = params.fetchFn ?? fetch;
+  const endpoint = params.endpoint.replace(/\/+$/, "");
+  const url = `${endpoint}/models?api-version=2024-05-01-preview`;
+
+  const discoveryPromise = (async () => {
+    const response = await fetchFn(url, {
+      method: "GET",
+      headers: {
+        "api-key": params.apiKey,
+        Accept: "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Azure Foundry model listing failed (${response.status})`);
+    }
+    const body = (await response.json()) as { data?: AzureFoundryModelEntry[] };
+    const entries = body.data ?? [];
+    const discovered: ModelDefinitionConfig[] = [];
+    for (const entry of entries) {
+      if (!matchesProviderFilter(entry, providerFilter)) {
+        continue;
+      }
+      const def = toModelDefinition(entry, {
+        contextWindow: defaultContextWindow,
+        maxTokens: defaultMaxTokens,
+        endpoint,
+      });
+      if (def) {
+        discovered.push(def);
+      }
+    }
+    return discovered.toSorted((a, b) => a.name.localeCompare(b.name));
+  })();
+
+  if (refreshIntervalSeconds > 0) {
+    discoveryCache.set(cacheKey, {
+      expiresAt: now + refreshIntervalSeconds * 1000,
+      inFlight: discoveryPromise,
+    });
+  }
+
+  try {
+    const value = await discoveryPromise;
+    if (refreshIntervalSeconds > 0) {
+      discoveryCache.set(cacheKey, {
+        expiresAt: now + refreshIntervalSeconds * 1000,
+        value,
+      });
+    }
+    return value;
+  } catch (error) {
+    if (refreshIntervalSeconds > 0) {
+      discoveryCache.delete(cacheKey);
+    }
+    if (!hasLoggedAzureFoundryError) {
+      hasLoggedAzureFoundryError = true;
+      log.warn(`Failed to list Azure Foundry models: ${String(error)}`);
+    }
+    return [];
+  }
+}

src/media-understanding/providers/azure/audio.ts
@@ -1,0 +1,116 @@
+import { createSubsystemLogger } from "../../../logging/subsystem.js";
+import { resolveAzureFoundryApiVersionEnv } from "../../../providers/azure-foundry/env.js";
+import type { AudioTranscriptionRequest, AudioTranscriptionResult } from "../../types.js";
+import {
+  assertOkOrThrowHttpError,
+  normalizeBaseUrl,
+  postTranscriptionRequest,
+  requireTranscriptionText,
+} from "../shared.js";
+
+const log = createSubsystemLogger("azure-audio");
+
+const DEFAULT_AZURE_AUDIO_BASE_URL = "https://models.inference.ai.azure.com";
+const DEFAULT_AZURE_AUDIO_MODEL = "whisper-large-v3-turbo";
+const DEFAULT_AZURE_API_VERSION = "2025-04-01-preview";
+
+function resolveModel(model?: string): string {
+  const trimmed = model?.trim();
+  return trimmed || DEFAULT_AZURE_AUDIO_MODEL;
+}
+
+/**
+ * Build the transcription URL for Azure Foundry.
+ *
+ * Two forms are supported:
+ *
+ * 1. **Full deployment URL** (baseUrl already contains `/openai/deployments/{name}`):
+ *    → append `/audio/transcriptions`
+ *
+ * 2. **Bare endpoint** (e.g. `https://models.inference.ai.azure.com`):
+ *    → append `/openai/deployments/{model}/audio/transcriptions`
+ */
+function buildTranscriptionUrl(baseUrl: string, model: string): URL {
+  const normalized = baseUrl.replace(/\/+$/, "");
+  if (/\/openai\/deployments\/[^/]+$/i.test(normalized)) {
+    // baseUrl already points to a specific deployment.
+    return new URL(`${normalized}/audio/transcriptions`);
+  }
+  // Strip OpenAI-compatible suffixes (e.g. /openai/v1, /v1) to get the bare endpoint.
+  const bare = normalized.replace(/\/openai\/v\d+$/i, "").replace(/\/v\d+$/i, "");
+  return new URL(`${bare}/openai/deployments/${encodeURIComponent(model)}/audio/transcriptions`);
+}
+
+export async function transcribeAzureAudio(
+  params: AudioTranscriptionRequest,
+): Promise<AudioTranscriptionResult> {
+  const fetchFn = params.fetchFn ?? fetch;
+  const baseUrl = normalizeBaseUrl(params.baseUrl, DEFAULT_AZURE_AUDIO_BASE_URL);
+  const allowPrivate = Boolean(params.baseUrl?.trim());
+
+  const model = resolveModel(params.model);
+  const url = buildTranscriptionUrl(baseUrl, model);
+
+  // Apply query params from providerOptions (e.g. api-version).
+  if (params.query) {
+    for (const [key, value] of Object.entries(params.query)) {
+      if (value === undefined) {
+        continue;
+      }
+      url.searchParams.set(key, String(value));
+    }
+  }
+  // Ensure api-version is always present.
+  if (!url.searchParams.has("api-version")) {
+    const apiVersion =
+      resolveAzureFoundryApiVersionEnv(process.env)?.value || DEFAULT_AZURE_API_VERSION;
+    url.searchParams.set("api-version", apiVersion);
+  }
+
+  const form = new FormData();
+  const fileName = params.fileName?.trim() || "audio";
+  const bytes = new Uint8Array(params.buffer);
+  const blob = new Blob([bytes], {
+    type: params.mime ?? "application/octet-stream",
+  });
+  form.append("file", blob, fileName);
+  form.append("model", model);
+  if (params.language?.trim()) {
+    form.append("language", params.language.trim());
+  }
+  if (params.prompt?.trim()) {
+    form.append("prompt", params.prompt.trim());
+  }
+
+  const headers = new Headers(params.headers);
+  // Remove any Bearer auth that may have been set by the caller.
+  headers.delete("authorization");
+  // Azure OpenAI transcription endpoints accept api-key (and optional Bearer token),
+  // including cognitiveservices and services.ai hosts.
+  headers.set("api-key", params.apiKey);
+
+  const { response: res, release } = await postTranscriptionRequest({
+    url: url.toString(),
+    headers,
+    body: form,
+    timeoutMs: params.timeoutMs,
+    fetchFn,
+    allowPrivateNetwork: allowPrivate,
+  });
+
+  try {
+    await assertOkOrThrowHttpError(res, "Azure audio transcription failed");
+
+    const payload = (await res.json()) as { text?: string };
+    const text = requireTranscriptionText(
+      payload.text,
+      "Azure audio transcription response missing text",
+    );
+    return { text, model };
+  } catch (err) {
+    log.warn(`Transcription failed for ${url.origin}${url.pathname}: ${String(err)}`);
+    throw err;
+  } finally {
+    await release();
+  }
+}